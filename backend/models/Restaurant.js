9fd1dc600eba1865a3658df3f53de255ab5034fd
Pritampriyam <166136790+Pritampriyam@users.noreply.github.com>
2025-07-19 23:02:54 +0530

    Create Restaurant.js

---
+++ backend/models/Restaurant.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+const RestaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  address: { type: String, required: true },
+  menu: [{ name: String, price: Number }]
+});
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
